9b4802494129c24f2e111accfe738b3a9374d354
Miron Bykov <miron.bykov@kcl.ac.uk>
2023-02-11 14:47:09 +0000

    initialise contract

---
+++ src/contracts/Adversary.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// States that we are open source with respect to the MIT open-source license
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Adversary is ERC721 {
+    struct Game {
+
+    }
+
+    constructor() ERC721(){}
+}
