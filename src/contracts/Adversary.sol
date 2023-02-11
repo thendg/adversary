bab85981857d9f97753aa12aa4fc924cdd4dd115
Miron Bykov <miron.bykov@kcl.ac.uk>
2023-02-11 17:02:56 +0000

    complete play functionality

--- src/contracts/Adversary.sol
+++ src/contracts/Adversary.sol
@@ -3,12 +3,52 @@
 
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract Adversary is ERC721 {
-    struct Game {
-
+contract Adversary {
+    // holds address and tokenID from the original contract (in which the nft was first minted)
+    struct Token {
+        uint256 tokenID;
+        address originalContract;
     }
 
-    constructor() ERC721(){}
+    struct Game {
+        address host;
+        address challenger;
+        Token[] prizePool;
+    }
+
+    address private admin;
+    uint256 private nextGameID;
+    mapping(uint256 => Game) public games;
+
+    constructor() {
+        admin = msg.sender;
+        nextGameID = 0;
+    }
+
+    function setNextID() private {
+        nextGameID++;
+    }
+
+    function play(
+        address host,
+        address challenger,
+        uint256[] memory tokenIDs,
+        address[] memory originalContracts
+    ) public onlyAdmin {
+        require(
+            tokenIDs.length == originalContracts.length,
+            "tokenID and address lists are not same length"
+        );
+
+        Token[] memory temporaryPool = new Token[](tokenIDs.length);
+        for (uint256 i = 0; i < tokenIDs.length; i++) {
+            temporaryPool[i] = (Token(tokenIDs[i], originalContracts[i]));
+        }
+        games[nextGameID] = Game(host, challenger, temporaryPool);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admins can call this function.");
+        _;
+    }
 }
