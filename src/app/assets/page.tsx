6fd3a67bbc300b3de74da5ed1f0b8beb68e821ce
chubbsbucket <74867406+chubbsbucket@users.noreply.github.com>
2023-02-12 04:34:08 +0000

    Revert "corrected feature"
    This reverts commit c164e748e2db615a99121789849dd4015a7aedd6.

--- src/app/assets/page.tsx
+++ src/app/assets/page.tsx
@@ -1,74 +1,41 @@
 "use client";
-import TokenSelector from '@/components/TokenSelector';
-import { ImmutableX, Config, IMXError} from '@imtbl/core-sdk';
+import { ImmutableX, Config } from '@imtbl/core-sdk';
 import {useRef, useState} from 'react';
 
 const config = Config.SANDBOX; // Or Config.PRODUCTION
 const client = new ImmutableX(config);
 
-type TokenInfo = {
-    address: string, 
-    id: string,
-    name: string
-}
-
 export default function Assets() {
     const [user, setUser] = useState('');
-    //const [tokens, setTokens] = useState();
-    
-    //for the addresses where the user has tokens
-    const [tokenArr, setTokenArr] = useState<TokenInfo[]>([]); 
-    
-    const inputRef = useRef<HTMLInputElement>(null);
-    const [checked, setChecked] = useState<{[fieldName: string]: boolean}>({});
+    const [tokens, setTokens] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null)
 
     //gets the data about tokens the user holds
-    const getListBalances = async () => {
+     const getListBalances = async () => {
         if (!inputRef.current) return
 
-        console.log(inputRef.current.value);
+        //console.log(inputRef.current.value);
 
         const owner = inputRef.current.value;
-        try {
-            const response = await client.listBalances({owner, })
-            var arr: TokenInfo[] = [];
-            response.result.forEach(async (item) => {
-                try {
-                    const response = await client.listAssets({
-                        collection: item.token_address, 
-                        orderBy: 'name',
-                    })
-                    const assetInformationList = response.result
-                    .filter((asset) => {
-                        if (!inputRef.current) return false;
-                        return asset.user == inputRef.current.value
-                    })
-                    .map((asset) =>{
-                       let NFTName = asset.name !== null ? asset.name : '';
-                       let token: TokenInfo = ({
-                        address : asset.token_address, 
-                        id: asset.token_id,
-                        name : NFTName
-                        })
-                        return token;
-                    })
-                    assetInformationList.forEach((item) => arr.push(item));
-                } catch(e: unknown) {
-                    console.log(e);
+        await client.listBalances(
+            {owner, }
+        ).then(
+            (response) => {
+                var formatted = '';
+                for(var i = 0; i < response.result.length; i++) {
+                    formatted += 'symbol: ' + response.result[i].symbol + '\n';
+                    formatted += 'balance: ' + response.result[i].balance + '\n';
+                    formatted += 'withdrawable: ' + response.result[i].balance + '\n';
+                    formatted += '\n';
                 }
-            });
-            setTokenArr(arr);
-        } catch(e: unknown) {
-            console.log(e);
-        }
-    }
-
-    const handleCheck = (key: string) => {
-        const newValue = !checked.key;
-        setChecked({...checked, [key]:newValue});
+                setTokens(formatted);
+            }
+        ).catch((e) => setTokens(e.message));
+        inputRef.current.value = '';
 
     }
 
+    
     return(
         <div className="font-bungee w-full h-screen flex flex-col justify-center text-5xl text-bold bg-gradient-to-r from-red-600 via-red-300 to-orange-500 background-animate">
             <input
@@ -77,21 +44,11 @@ export default function Assets() {
                     placeholder="Username"
                     ref={inputRef}
                     className="text-center ml-32 mr-32 text-2xl"
-            />
-            <button onClick={() => {getListBalances();}} className="text-2xl ml-32 mr-32">Submit</button>
+                />
+            <button onClick={() => getListBalances()} className="text-2xl ml-32 mr-32">Submit</button>
             <div className="flex flex-col justify-center text-2xl text-bold text-center">
-                <h1 id="tokens">Tokens:</h1>  
-                {tokenArr.map((item) => <div>
-                        <div className="flex flex-row justify-center mr-64 ml-64">
-                        <input onChange={() => handleCheck(item.address + item.id)} className="" type="checkbox"/>
-                        <label>
-                            <h1 className="text-2xl">{item.name}</h1>
-                            <h2 className="text-1xl">{item.address}</h2>
-                        </label>
-                        </div>
-                    </div>)
-                }
-                <button>Choose</button>
+                <h1>Tokens:</h1>  
+                <p className="whitespace-pre-line">{tokens}</p>
             </div>
         </div>
     );
